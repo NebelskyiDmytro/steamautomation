47776aeeb80276b0bfe89297d1f144f53408597c
Dmytro Nebelskyi <dmytro.nebelskyi@gmail.com>
2021-10-18 14:07:27 +0300

    MAJOR - Singleton pattern added

--- src/main/java/common/CommonActions.java
+++ src/main/java/common/CommonActions.java
@@ -12,27 +12,28 @@ import static common.Config.*;
 import static constants.Constant.TimeoutVariables.IMPLICIT_WAIT;
 
 public class CommonActions {
+    static WebDriver driver = null;
 
-    public static WebDriver createDriver() {
+    public static synchronized WebDriver createDriver() {
+        if (driver == null) {
 
-        WebDriver driver = null;
-        ChromeOptions options = new ChromeOptions();
+            ChromeOptions options = new ChromeOptions();
 
-        switch (PLATFORM_AND_BROWSER) {
-            case "win_chrome":
-                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-                System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, String.valueOf(CHROME_CLIENT_LOG));
-                options.addArguments("--lang=" + CHROME_LANGUAGE);
-                driver = new ChromeDriver(options);
-                break;
-            default:
-                Assert.fail("Incorrect value of PLATFORM_AND_BROWSER: " + PLATFORM_AND_BROWSER);
+            switch (PLATFORM_AND_BROWSER) {
+                case "win_chrome":
+                    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                    System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, String.valueOf(CHROME_CLIENT_LOG));
+                    options.addArguments("--lang=" + CHROME_LANGUAGE);
+                    driver = new ChromeDriver(options);
+                    break;
+                default:
+                    Assert.fail("Incorrect value of PLATFORM_AND_BROWSER: " + PLATFORM_AND_BROWSER);
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
         }
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
         return driver;
-
     }
 
 }
